670c56f18d0f4b1823e0d81bb85d05a7e5803607
J Daniel Ritchie <dan.ritchie@hpe.com>
2017-02-24 09:47:50 -0800

    First cut at customizing REST API server

--- pkg/config/jobber_config.go
+++ pkg/config/jobber_config.go
@@ -2,11 +2,19 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"strings"
+)
+
+const (
+	jobberHost string = "127.0.0.1"
+	jobberPort int32  = 9090
 )
 
 // JobberConfig hlods the config parameters for the Jobber service
 type JobberConfig struct {
+	Host      string
+	Port      int32
 	Cassandra CassandraConfig
 }
 
@@ -17,11 +25,34 @@ type CassandraConfig struct {
 }
 
 // ReadConfig reads in the jobber service configuration
-func ReadConfig() JobberConfig {
+func ReadConfig() (JobberConfig, []error) {
+	return getJobberEnvConfig()
+}
+
+func getJobberEnvConfig() (JobberConfig, []error) {
+	errors := []error{}
 	config := JobberConfig{
+		Host:      jobberHost,
+		Port:      jobberPort,
 		Cassandra: getCassandraEnvConfig(),
 	}
-	return config
+	envVars := os.Environ()
+	for _, envVar := range envVars {
+		parts := strings.Split(envVar, "=")
+		switch parts[0] {
+		case "JOBBER_HOST":
+			config.Host = parts[1]
+		case "JOBBER_PORT":
+			value, err := strconv.ParseInt(parts[1], 10, 32)
+			if err != nil {
+				errors = append(errors, err)
+			} else {
+				config.Port = int32(value)
+			}
+		default:
+		}
+	}
+	return config, errors
 }
 
 func getCassandraEnvConfig() CassandraConfig {
